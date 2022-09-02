d50b2b9784a6053c1b6479b79b8f8c34949d8016
Michael Schilonka <michael@blueshoe.de>
2022-09-02 17:52:23 +0200

    feat: first step; adds binary

--- out/extension.js
+++ out/extension.js
@@ -4,19 +4,21 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const child_process_1 = require("child_process");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gefyra" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('gefyra.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from Gefyra!');
+    console.log("ensure binary exists for correct platform; downloaded from GitHub release");
+    console.log(context.extensionPath);
+    let disposable = vscode.commands.registerCommand('gefyra.up', () => {
+        const gefyra = (0, child_process_1.spawn)(context.extensionPath + '/gefyra-json', ['{"action": "gefyra.status"}']);
+        gefyra.stdout.on("data", data => {
+            console.log(`stdout: ${data}`);
+        });
+        gefyra.on("error", data => {
+            console.error(data);
+        });
+        vscode.window.showInformationMessage('Now starting Gefyra');
     });
     context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log("ensure binary exists for correct platform; downloaded from GitHub release");
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gefyra" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gefyra.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Gefyra!');
+	console.log(context.extensionPath);
+	let disposable = vscode.commands.registerCommand('gefyra.up', () => {
+		
+		const gefyra = spawn(context.extensionPath + '/gefyra-json', ['{"action": "gefyra.status"}']);
+		gefyra.stdout.on("data", data => {
+			console.log(`stdout: ${data}`);
+		});
+		gefyra.on("error", data => {
+			console.error(data);
+		});
+		
+		vscode.window.showInformationMessage('Now starting Gefyra');
 	});
 
 	context.subscriptions.push(disposable);
